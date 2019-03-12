bbfcaf59917a06ce6f7490d68517858788b4e9eb
Gideon <GideonHarmse0404@gmail.com>
2019-03-12 23:14:55 +0200

    .

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
     songs: forwardTo('prisma'),
     songs2(parent, args, { prisma }, info) {
-        throw new Error('You must be logged in!');
+        console.log('Fetch');      
         return prisma.query.songs(args, info)
     },
 }
